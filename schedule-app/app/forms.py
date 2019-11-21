75678285596c9f2396057b91f3bcfbb4279c80ee
Mitch Jones <mitch@mitchelljones.com>
2019-11-21 13:40:12 -0500

    Add reCaptcha verification

--- schedule-app/app/forms.py
+++ schedule-app/app/forms.py
@@ -1,18 +1,16 @@
-from flask_wtf import FlaskForm
+from flask_wtf import RecaptchaField, Recaptcha, FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, InputRequired, Email, Length, EqualTo, ValidationError
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=64,
-                                                                           message="Username must be between 4 and 64 characters long.")])
-    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=128,
-                                                                             message="Password must be between 8 and 128 characters long.")])
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=64, message="Username must be between 4 and 64 characters long.")])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=128, message="Password must be between 8 and 128 characters long.")])
     remember = BooleanField('Remember Me')
 
 
 class SignupForm(FlaskForm):
-    email = StringField('Email', validators=[InputRequired(), Email(message='Invalid email'),
-                                                     Length(max=64, message="Email must be less than 64 characters.")])
+    email = StringField('Email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=64, message="Email must be less than 64 characters.")])
     username = StringField('Username', validators=[InputRequired(), Length(min=4, max=64, message="Username must be between 4 and 64 characters long.")])
     password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=128, message="Password must be between 8 and 128 characters long.")])
     password2 = PasswordField('Retype Password', validators=[DataRequired(), EqualTo('password', message="Passwords must match")])
+    recaptcha = RecaptchaField(validators=[Recaptcha(message="Recaptcha must be completed")])
--- schedule-app/config.py
+++ schedule-app/config.py
@@ -3,4 +3,8 @@ import os
 class Config(object):
     SECRET_KEY = 'YBieLEwbUBGbThmVeLZaiP8a8L3DPp'
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(os.path.abspath(os.path.dirname(__file__)), 'users.db')}"
+    RECAPTCHA_USE_SSL = True
+    RECAPTCHA_PUBLIC_KEY = '6Lem_cMUAAAAAKJkYy8dzRKOrZAGMXg1oztfP1id'
+    RECAPTCHA_PRIVATE_KEY = '6Lem_cMUAAAAADtjIVxrSOeFnAI8uSTyhEz1t7Eg'
+    RECAPTCHA_OPTIONS = {'theme': 'black'}
 
