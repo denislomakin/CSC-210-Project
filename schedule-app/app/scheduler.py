680925fc89660cd65dd9deb5818056b80c1d5a79
denislomakin <36579680+denislomakin@users.noreply.github.com>
2019-12-09 17:03:07 -0500

    add ScheduleOverlap

--- schedule-app/app/scheduler.py
+++ schedule-app/app/scheduler.py
@@ -13,6 +13,35 @@ class Schedule:
 		self.times = [datetime.strftime(date, "%I:%M %p") for date in 
 					 		datetime_range(start_time_parsed, end_time_parsed,
 					 		timedelta(minutes=15))]
+							
+class ScheduleOverlap:
+	def __init__(self, schedule, avail_arr):
+		id_list = []
+		for time in schedule.times:
+			for date in schedule.dates:
+				id_list.append(date + " " + time)
+
+		overall_avail = {}
+		avail_max = 0
+		for id1 in id_list:
+			overall_avail[id1] = 0
+		for availability in avail_arr:
+			for id1 in id_list:
+				if availability[id1]:
+					overall_avail[id1] += 1
+					if overall_avail[id1] > avail_max:
+						avail_max = overall_avail[id1]
+
+		full_avail = Color("#5f7eed")
+		no_avail = Color("#f0f0f0")
+		colors = list(no_avail.range_to(full_avail, avail_max))
+
+		color_dict = {}
+		for id1 in id_list:
+			color_dict[id1] = colors[overall_avail[id1]]
+
+		return color_dict
+
 
 def datetime_range(start, end, delta):
 	current = start
